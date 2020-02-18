5818276b3a1a2d127f214d7cd9d4bb98c34ea2ea
Ramazan Cetin <ramazan.cetin@kentkart.com.tr>
2020-02-18 16:53:00 +0300

    m

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const app = express();
 
-app.use('/', function (req, res) {
+app.get('/', function (req, res) {
     return res.json("Hello World!")
 })
 
--- src/server.js
+++ src/server.js
@@ -1,8 +1,7 @@
 const app  = require("./app")
 
 const PORT = 8005
-const HOST = "0.0.0.0"
 
-app.listen(PORT, HOST, function () {
+app.listen(PORT, function () {
     console.log(`Server listens on port: ${PORT}`);
 })

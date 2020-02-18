d63e485d72ba611bb4274c3778c44365ac2b2ff6
RC <ramazancetinn@yandex.com>
2020-02-18 22:30:22 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get('/', function (req, res) {
-    return res.json("Hello Samir Omar!1")
+    return res.json("Hello Ramo!1")
 })
 
 module.exports = app

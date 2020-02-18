fcd37bbeb48cfbdd2669ad2e1f81536a7ce0649c
Ramazan Cetin <ramazan.cetin@kentkart.com.tr>
2020-02-18 17:58:08 +0300

    news

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get('/', function (req, res) {
-    return res.json("Hello From Jenkins!!")
+    return res.json("Hello From Jenkins v2!!")
 })
 
 module.exports = app
